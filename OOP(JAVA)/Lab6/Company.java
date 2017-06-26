750cc60b45f57634b6c562169673a7fa6b5e3de3
Navneet Kishan <itsnavneetk@gmail.com>
2017-06-26 23:27:30 +0530

    Adding Java lab6 programs

---
+++ OOP(JAVA)/Lab6/Company.java
@@ -0,0 +1,24 @@
+package lab_6;
+import java.util.*;
+
+
+interface Comapany1{
+    public void calculate();
+}
+class Hardware implements Company1{
+    int i;
+    String[] category = new String[4];
+    String[] manuf = new String[4];
+    Scanner sc = new Scanner(System.in);
+    for(i=0;i<4;i++){
+   
+}
+    
+    void calculate(){
+        
+}
+    
+}
+class Software{
+    String type,OS;
+}
---
+++ OOP(JAVA)/Lab6/Company1.java
@@ -0,0 +1,24 @@
+package lab_6;
+import java.util.*;
+
+
+interface Comapany{
+    void calculate();
+}
+class Hardware{
+    int i;
+    String[] category = new String[4];
+    String[] manuf = new String[4];
+    Scanner sc = new Scanner(System.in);
+    for(i=0;i<4;i++){
+   
+}
+    
+    void calculate(){
+        
+}
+    
+}
+class Software{
+    String type,OS;
+}
